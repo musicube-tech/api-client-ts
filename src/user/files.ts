ff53fff7d50c965462a58c0d82aadbb8e87855e3
Xiphe <github@xiphe.net>
2021-10-04 15:32:40 +0200

    fix: support more filters for files

--- src/user/files.ts
+++ src/user/files.ts
@@ -3,10 +3,21 @@ import { config } from '../config';
 import type { File, ApiFile } from '../types';
 
 export interface Filters {
+  name?: string | null;
   contributor?: string | null;
   mainArtist?: string | null;
   composer?: string | null;
   genre?: string | null;
+  folder?: string | null;
+  extension?: string | null;
+  fingerprintStatus?: string | null;
+  inferenceStatus?: string | null;
+  uploadStartTime?: Date;
+  uploadEndTime?: Date;
+  inferenceStartTime?: Date;
+  inferenceEndTime?: Date;
+  fingerprintStartTime?: Date;
+  fingerprintEndTime?: Date;
   musicalFeatures?: Record<string, string | null>;
 }
 export interface Options {
@@ -28,10 +39,13 @@ export async function files(
 ): Promise<File[]> {
   const filtersWithValue = Object.fromEntries(
     Object.entries(filters)
+      .map(([k, v]: [string, string | null | undefined | Date]) => [
+        k,
+        v instanceof Date ? v.toISOString() : v,
+      ])
       .concat(Object.entries(musicalFeatures))
-      .filter(
-        (entry): entry is [string, string] =>
-          entry[1] !== null && entry[1].length > 0,
+      .filter((entry): entry is [string, string] =>
+        Boolean(entry[1] && entry[1].length > 0),
       ),
   );
   const query = new URLSearchParams({
